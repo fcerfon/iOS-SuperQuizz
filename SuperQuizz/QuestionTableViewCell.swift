0fa96b965b8f77f3018aa820c201495bd1fc8a33
fcerfon <florent.cerfon@gmail.com>
2018-12-04 15:19:18 +0100

    show list of 6 mock questions

---
+++ SuperQuizz/QuestionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionTableViewCell.swift
+//  SuperQuizz
+//
+//  Created by formation 1 on 04/12/2018.
+//  Copyright © 2018 formation 1. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
